e49b36673b4a54db373831af6a46e2269cd38402
viclm <vic285011@gmail.com>
2011-05-26 01:15:52 +0800

    chrome dict

--- chromeapp/PowerWord 2.0/src/contentscript.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var contentscript = document.createElement('script');
-    contentscript.type = 'text/javascript';
-    contentscript.src = chrome.extension.getURL('src/ui.js');
-    contentscript.charset = 'UTF-8';
-    document.body.appendChild(contentscript);
-})();
