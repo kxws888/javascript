6eebcb3fc443798e78ffefbe827bacba921adbc6
viclm <vic285011@gmail.com>
2011-06-03 00:15:38 +0800

    2as

--- chromeapp/ChromeDict/src/iciba_param.js
+++
@@ -1,7 +0,0 @@
-var iciba_param = {};
-
-chrome.extension.sendRequest({action: 'config'}, function (response) {
-    localStorage['iciba_param'] = response;
-    chrome.extension.sendRequest({action: 'init ui'});
-});
-
