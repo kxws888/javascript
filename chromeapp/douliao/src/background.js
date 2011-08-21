0c11a4239a52606e8f29f822814b321cc5a8c36a
viclm <fliuming@gmail.com>
2011-08-21 23:17:11 +0800

    douliao

---
+++ chromeapp/douliao/src/background.js
@@ -0,0 +1,266 @@
+'use strict';
+
+oauth();
+
+function Resource(args) {
+    this.method = args.method || 'get';
+    this.url = args.url || '';
+    this.params = args.params //this.setParams(args.params);
+    this.data = args.data || null;
+    this.load = args.load || function () {};
+    this.error = args.error || function () {};
+}
+
+Resource.prototype.setParams = function (params) {
+    var obj = {}, key;
+    for (key in params) {
+        obj[key] = params[key];
+    }
+    return obj;
+}
+
+Resource.prototype.oauth = function () {
+    var message, self = this;
+    message = {
+        method: self.method,
+        action: self.url,
+        parameters: {
+            oauth_consumer_key: window.localStorage.getItem('consumer_key'),
+            oauth_token: window.localStorage.getItem('access_token'),
+            oauth_signature_method: window.localStorage.getItem('signature_method'),
+            oauth_signature: '',
+            oauth_timestamp: '',
+            oauth_nonce: ''
+        }
+    }
+
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, {
+        consumerSecret: window.localStorage.getItem('consumer_key_secret'),
+        tokenSecret: window.localStorage.getItem('access_token_secret')
+    });
+
+    return OAuth.getAuthorizationHeader(message.action, message.parameters);
+}
+
+Resource.prototype.stringify = function (parameters) {
+    var params = [];
+    for(var p in parameters) {
+        params.push(encodeURIComponent(p) + '=' + encodeURIComponent(parameters[p]));
+    }
+    return params.join('&');
+},
+
+Resource.prototype.request = function () {
+    var xhr = new XMLHttpRequest(), self = this, data;
+    //data = this.stringify(this.params);
+    if (this.method.toLowerCase() === 'get' && this.data) {
+        this.url += '?' + this.data;
+    }
+    xhr.onload = function (e) {
+        if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+            self.load.call(self, xhr.responseText, e);
+        }
+        else {
+            self.error.call(self, xhr, e);
+        }
+    }
+    xhr.onerror = function (e) {
+        self.error.call(self, e);
+    }
+    xhr.open(this.method, this.url, true);
+    xhr.setRequestHeader('content-type', 'application/atom+xml');
+    xhr.setRequestHeader('Authorization', this.oauth());
+    xhr.send(this.data);
+}
+
+
+
+
+function Mail(args) {
+
+    var self = this;
+
+    this.people = [];
+    this.filterRegTest = /说:[\r\n]+\|/m;
+    this.filterRegFront = /^([\s\S]+?[\r\n])?[^\r\n]+?说:[\r\n]+\|/m;
+    this.filterRegBack = /^[\s\S]+[\r\n]\|.+?[\r\n]+([\s\S]+)$/m;
+
+    chrome.extension.onConnect.addListener(function(port) {
+        //port.sender.tab
+        if (port.name === 'dchat') {
+            port.onMessage.addListener(function(msg) {
+                switch (msg.cmd) {
+                    case 'send':
+                        self.send(msg, function (data, e) {console.log(data)
+                                  if (data === 'ok') {
+                                      port.postMessage({cmd: 'sended', result: true});
+                                  }
+                    }, function (e) {
+                        if (e.status === 403) {
+                            port.postMessage({
+                                cmd: 'sended',
+                                result: false,
+                                msg:{
+                                    content: msg.content,
+                                    people: msg.people,
+                                    captcha: {
+                                        token: /=(.+?)&amp;/.exec(e.responseText),
+                                        string: /captcha_url=(.+)$/.exec(e.responseText)
+                                    }
+                                }
+                            });
+                        }
+                        //port.postMessage({cmd: 'sended', result: false});
+                    });
+                    break;
+                    case 'receivestart':
+                        if (self.people.indexOf(msg.people) === -1) {
+                        self.people.push(msg.people);
+                    }
+                    if (self.people.length > -1) {
+                        self.receiveStart(port);
+                    }
+                    break;
+                }
+            });
+
+            port.onDisconnect.addListener(function (port) {
+                if (port.name === 'dchat') {
+                    self.people.splice(self.people.indexOf(port.tab.url.match(/\/([^\/]+)\/?$/)[1]), 1);
+                    if (self.people.length === 0) {
+                        clearInterval(self.timer);
+                    }
+                }
+            });
+        }
+    });
+
+    chrome.extension.onConnect.addListener(function(port) {
+        //port.sender.tab
+        if (port.name === 'dchat') {
+
+        }
+    });
+}
+
+Mail.prototype.send = function (msg, load, error) {
+    var entry = '<?xml version="1.0" encoding="UTF-8"?>'
+    +'<entry xmlns="http://www.w3.org/2005/Atom" xmlns:db="http://www.douban.com/xmlns/" xmlns:gd="http://schemas.google.com/g/2005" xmlns:opensearch="http://a9.com/-/spec/opensearchrss/1.0/">'
+        +'<db:entity name="receiver">'
+    +'<uri>http://api.douban.com/people/' + msg.people + '</uri>'
+        +'</db:entity>'
+    +'<content>' + msg.content + '</content>'
+    +'<title>通过豆聊发送的消息</title>'
+    +(msg.captcha ? ('<db:attribute name="captcha_token">' + msg.captcha.token + '</db:attribute><db:attribute name="captcha_string">' + msg.captcha.string + '</db:attribute>') : '')
+    +'</entry>', self;
+
+    new Resource({
+        url: 'http://api.douban.com/doumails',
+            method: 'post',
+        data: entry,
+        load: load,
+        error: error
+    }).request();
+}
+
+Mail.prototype.receive = function (load, error) {
+    var self = this;
+    new Resource({
+        url: 'http://api.douban.com/doumail/inbox/unread',
+            method: 'get',
+        data: 'start-index=1&alt=json',
+        load: load,
+        error: error
+    }).request();
+}
+
+
+Mail.prototype.receiveStart = function (port) {
+    var self = this;
+    self.timer = setInterval(function () {
+        self.receive(function (data, e) {
+            var i, len, key, people, mails = [];
+            data = JSON.parse(data).entry;
+            for (i = 0, len = data.length ; i < len ; i += 1) {
+                people = data[i].author.link[1]['@href'].match(/\/([^\/]+)\/?$/)[1];
+                if (self.people.indexOf(people) > -1) {
+                    mails[mails.length] = data[i].id['$t'];
+                }
+            }
+            for (i = 0, len = mails.length ; i < len ; i += 1) {
+                new Resource({
+                    url: mails[i],
+                    method: 'get',
+                    data: 'alt=json',
+                    load: function (data) {
+                        data = JSON.parse(data);
+                        var response = {}, str1, str2;
+                        response.cmd = 'received';
+                        response.people = data.author.link[1]['@href'].match(/\/([^\/]+)\/?$/)[1];
+                        response.timestamp = data.published['$t'];
+
+                        str1 = data.content['$t'].trim();
+                        if (self.filterRegTest.test(str1)) {
+                            str2 = self.filterRegFront.exec(str1)[1];
+                            if (typeof str2 === 'undefined') {
+                                str2 = self.filterRegBack.exec(str1)[1];
+                                if (typeof str2 === 'undefined') {
+                                    str2 = '';
+                                }
+                            }
+                        }
+                        else {
+                            str2 = str1;
+                        }
+                        response.content = str2;console.log(str1, str2)
+                        port.postMessage(response);
+                        self.nofify(str2);
+                    }
+                }).request();
+            }
+        }, function (e) {
+            console.log(e)
+        });
+    }, 30000);
+}
+
+Mail.prototype.nofify = function (content) {
+    var notification = webkitNotifications.createNotification('../assets/icon16.png', '来新豆聊了', content);
+    notification.show();
+    setTimeout(function () {
+        notification.cancel();
+    }, 3000);
+}
+
+
+/*
+Mail.prototype.getMe = function (callback) {
+var self = this;
+new Resource({
+url: 'http://api.douban.com/people/%40me',
+method: 'get',
+data: 'alt=json',
+load: function (data, e) {
+data = JSON.parse(data);
+self.me = data['db:uid']['$t'];
+callback(true);
+},
+error: function (e) {
+chrome.tabs.create({url: 'pages/log.html'}, function (tab) {
+chrome.tabs.sendRequest(tab.id, e);
+});
+}
+}).request();
+}
+*/
+
+
+var doumail = new Mail();
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.status !== 'complete' && tab.url.indexOf('www.douban.com/people/') > -1) {
+        chrome.tabs.insertCSS(tabId, {file: 'pages/style/ui.css'});
+        chrome.tabs.executeScript(tabId, {file: "src/contentscript.js"});
+        chrome.pageAction.show(tab.id);
+    }
+});
---
+++ chromeapp/douliao/src/contentscript.js
@@ -0,0 +1,166 @@
+(function () {
+    var container = document.querySelector('#profile .user-opt'), button,
+    people, port = null, lock = false, lastTime = +new Date(),
+    chatWindow, textbox, msgList;
+    button = container.querySelector('a.mr5').cloneNode(false);
+    button.innerHTML = '豆聊';
+    button.style.marginLeft = '5px';
+    container.insertBefore(button, document.getElementById('divac'));
+
+    button.addEventListener('click', chatStart, false);
+
+
+
+    function chatStart(e) {
+        e.preventDefault();
+        if (port !== null) {return;}
+        if (!chatWindow) {
+            chatWindow = createChatWindow();
+            textbox = chatWindow.querySelector('textarea');
+            msgList = chatWindow.querySelector('section>div');
+            document.getElementById('close').addEventListener('click', function (e) {
+                chatWindow.style.display = 'none';
+                //port.postMessage({cmd: 'receivestop', people: people});
+                port.disconnect();
+                port = null;
+                e.stopPropagation();
+            }, false);
+            textbox.addEventListener('keyup', send, false);
+            msgList.addEventListener('scroll', function () {
+                this.scrollTop = this.scrollHeight;
+            }, false);
+
+            people = location.href.match(/\/([^\/]+)\/?$/)[1];
+        }
+        else {
+            chatWindow.style.display = '';
+        }
+
+        port = chrome.extension.connect({name: 'dchat'});
+        port.postMessage({cmd: 'receivestart', people: people});
+        port.onMessage.addListener(function (msg) {
+            if (msg.cmd === 'sended') {
+                if (!msg.result) {
+                    var tokenstring = document.getElementById('tokenstring');
+                    if (tokenstring) {
+                        tokenstring.find('img').src = msg.msg.captcha.string;
+                        msgList.querySelector('div:last-of-type').appendChild(tokenstring);
+                    }
+                    else {
+                        msgList.querySelector('div:last-of-type').appendChild(createCaptcha(msg.msg.captcha.string));
+                    }
+                    lock = false;
+                    textbox.bak = msg.msg;
+                }
+                else {
+                    if (textbox.bak) {
+                        delete textbox;
+                        msgList.removeChild(document.getElementById('tokenstring'));
+                    }
+                }
+            }
+            else if (msg.cmd === 'received') {
+                if (+new Date(msg.timestamp) > lastTime && people === msg.people) {
+                    var item = document.createElement('div');
+                    item.innerHTML = '<strong>' + document.querySelector('title').innerHTML.trim() + '说</strong>: ' + msg.content;
+                    msgList.appendChild(item);
+                    lock = false;
+                }
+            }
+        });
+    }
+
+    function createChatWindow() {
+        var aside = document.createElement('aside');
+        aside.id = 'dchat';
+        aside.innerHTML = '<header><h1></h1><div><img id="min" /><img id="close" /></div></header><section><div></div><div><form> <textarea id="" name="" rows="10" cols="30"></textarea></form></div></section>';
+        //aside.style.top = window.innerHeight - 255 + 'px';
+        document.body.appendChild(aside);
+        document.getElementById('min').src = drawMin();
+        document.getElementById('close').src = drawClose();
+        aside.querySelector('h1').innerHTML = document.querySelector('title').innerHTML.trim();
+        aside.querySelector('header').addEventListener('click', function () {
+            var section = this.nextSibling;
+            if (getComputedStyle(section, null).getPropertyValue('display') === 'block') {
+                section.style.display = 'none';
+            }
+            else {
+                section.style.display = 'block';
+            }
+        }, false);
+        return aside;
+    }
+
+    function createCaptcha(url) {
+        var div = document.createElement('div'), p, img;
+        p = document.createElement('p');
+        p.appendChild(document.createTextNode('发送太快了亲，输入验证码'));
+        img = document.createElement('img');
+        img.src = url;
+        div.appendChild(p);
+        div.appendChild(img);
+        div.id = 'tokenstring';
+        return div;
+    }
+
+    function send(e) {
+        if (e.keyCode !== 13) {return;}
+        if (lock) {
+            var log = document.createElement('p');
+            log.innerHTML = '别着急, 先等回复啊亲';
+            msgList.querySelector('div:last-of-type').appendChild(log);
+        }
+        else {
+            lock = true;
+            if (this.bak) {
+                var bak = this.bak, self = this;
+                port.postMessage({cmd: 'send', content: bak.content, people: bak.people, captcha: {token: bak.captcha.token, string: self.value}});
+            }
+            else {
+                var item = document.createElement('div');
+                item.innerHTML = '<strong>我说</strong>: ' + this.value;
+                msgList.appendChild(item);
+                port.postMessage({cmd: 'send', content: this.value, people: people});
+            }
+
+            this.value = '';
+            lastTime = +new Date();
+
+        }
+        e.preventDefault();
+        return false;
+    }
+
+    function drawClose() {
+        var canvas = document.createElement('canvas'), ctx;
+        canvas.width = 100;
+        canvas.height = 100;
+        canvas.style.backgroundColor = 'rgba(0, 0, 0, 1)';
+        ctx = canvas.getContext('2d');
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = '#0C7823';
+        ctx.beginPath();
+        ctx.moveTo(5,5);
+        ctx.lineTo(95, 95);
+        ctx.moveTo(95, 5);
+        ctx.lineTo(5, 95);
+        ctx.stroke();
+        return canvas.toDataURL();
+    }
+
+    function drawMin() {
+        var canvas = document.createElement('canvas'), ctx;
+        canvas.width = 100;
+        canvas.height = 100;
+        canvas.style.backgroundColor = 'rgba(0, 0, 0, 1)';
+        ctx = canvas.getContext('2d');
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = '#0C7823';
+        ctx.beginPath();
+        ctx.moveTo(5,85);
+        ctx.lineTo(95, 85);
+        ctx.stroke();
+        return canvas.toDataURL();
+    }
+
+})();
