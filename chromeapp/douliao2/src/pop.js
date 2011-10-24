9aa8705da2682e0286d0df281a037ef54120715b
viclm <fliuming@gmail.com>
2011-10-24 17:11:51 +0800

    ..

--- chromeapp/douliao2/src/pop.js
+++
@@ -1,6 +0,0 @@
-chrome.extension.sendRequest({cmd: 'getPop'}, function(response) {
-    document.title = response.name;
-    var dchat = new DChat({people: response.people, name: response.name, ui: 'simple'});
-    dchat.start();
-    dchat.chatWindow.querySelector('section>div').innerHTML = response.history;
-});
