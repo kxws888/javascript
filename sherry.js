1b1b03837d2b69640209fd1aefad25a5385154da
viclm <viclm@live.com>
2011-12-12 18:05:48 +0800

    sherry.js

---
+++ sherry.js
@@ -0,0 +1,42 @@
+var Sherry = {
+
+    extend: function (childCtor, parentCtor) {
+        var fnTest = /\bsuperclass\b/, parent = parentCtor.prototype
+        function tempCtor() {};
+        if (parent.superclass && !parent.multiSuperclass) {
+            parent.multiSuperclass = true;
+            for (var name in parent) {
+                if (parent.hasOwnProperty(name) && fnTest.test(parent[name])) {
+                    parent[name] = (function (name, fn) {
+                        return function () {
+                            var bak = this.superclass[name];
+                            this.superclass[name] = parent.superclass[name];
+                            var res = fn.apply(this, arguments);
+                            this.superclass[name] = bak;
+                            return res;
+                        }
+                    })(name, parent[name]);
+                }
+            }
+        }
+        tempCtor.prototype = parent;
+        childCtor.prototype = new tempCtor();
+        childCtor.prototype.superclass = parentCtor.prototype;
+        childCtor.prototype.constructor = childCtor;
+    },
+
+    proxy: function (fn, obj) {
+        return function () {
+            return fn.apply(obj, arguments);
+        }
+    },
+
+    clone: function (destination, source) {
+        for (var key in source) {
+            destination[key] = source[key];
+        }
+        return destination;
+    }
+};
+
+var S = Sherry;
